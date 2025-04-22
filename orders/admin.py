3e12472fc2090db983b618d9fcc731955b48dcb0
ali <aghorbani778@gmail.com>
2025-04-22 04:20:18 +0330

    some fields added  to admin.py

--- orders/admin.py
+++ orders/admin.py
@@ -13,6 +13,9 @@ class OrderAdmin(admin.ModelAdmin):
     model = Order
     list_display = ['user', 'is_paid', 'date_time_created']
     inlines = [ItemInline, ]
+    list_per_page = 20
+    search_fields = ["iuserd", 'date_time_created', ]
+    list_filter = ['user', 'is_paid', 'date_time_created', ]
 
 
 class OrderItemAdmin(admin.ModelAdmin):
